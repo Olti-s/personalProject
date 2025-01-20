cca54e5a7bcfe7aa2c341f97450af61fdf31978a
olti <sogojevaolti451@gmail.com>
2025-01-20 18:45:39 +0100

    ..

--- index.php
+++ index.php
@@ -60,6 +60,47 @@ button{
 button:hover{
     background-color:#123793;
 }
+
+
+.main{
+    width: 100%;
+    position: absolute;
+    text-align: center;
+    color: #fff;
+    background-image:;
+   
+    height: 100%;
+
+     }
+ 
+
+
+.main h1{
+    font-size: 70px;
+    margin-top: 80px;
+    color: #123793;
+}
+
+.main p{
+    margin: 20px auto;
+    font-weight: 100;
+    line-height: 25px;
+}
+
+.main-button{
+
+    width:200px;
+    padding: 15px 0;
+    text-align: center;
+    color: #fff;
+    background: transparent;
+    border: 2px solid #123793;
+    font-weight: bold;
+    border-radius: 25px;
+    margin: 0;
+
+}
+
     </style>
 </head>
 <body>
@@ -79,6 +120,25 @@ button:hover{
         <a class="button" href="login.php"> <button>SIGN IN</button></a>
         
     </header>
+    
+    
+   <main>
+
+
+
+    <div class="contanier">
+    <div class="main"> 
+        <h1>Find your style inspiration</h1>
+        <button class="main-button"><span></span>BUY NOW</button>
+        <button class="main-button"><span></span>MORE INFORMATION</button>
+    </div>
+    </div>
+</main>
+   
+
+
 </body>
+
+
 </html>
 
