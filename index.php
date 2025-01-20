02ee850db189c2278320025707f2f5bf041ac6f8
olti <sogojevaolti451@gmail.com>
2025-01-20 20:09:58 +0100

    ...

--- index.php
+++ index.php
@@ -44,12 +44,12 @@
 }
 
 .nav-links li a:hover{
-    color:#0088a9;
+    color:pink;
 }
 
 button{
     padding: 9px 25px;
-   background-color:#123793;
+   background-color:pink;
    border: none;
    border-radius: 50px;
    transition: all 0,3s ease 0s;
@@ -58,7 +58,7 @@ button{
 }
 
 button:hover{
-    background-color:#123793;
+    background-color:pink;
 }
 
 
@@ -67,9 +67,9 @@ button:hover{
     position: absolute;
     text-align: center;
     color: #fff;
-    background-image:;
+    background-image:url(bg1.jpg);
    
-    height: 100%;
+    height: 63%;
 
      }
  
@@ -92,12 +92,112 @@ button:hover{
     width:200px;
     padding: 15px 0;
     text-align: center;
-    color: #fff;
+    color: black;
     background: transparent;
     border: 2px solid #123793;
     font-weight: bold;
     border-radius: 25px;
     margin: 0;
+
+}
+.sale-price {
+    color: red;
+    font-weight: bold;
+    font-size: 1.5em;
+}
+.regular-price {
+    color: gray;
+    text-decoration: line-through;
+    font-size: 0.8em;
+    display: block;
+    margin-top: -10px;
+}
+.title {
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 30px;
+}
+.description {
+    line-height: 1.4;
+    margin-bottom: 20px;
+}
+.features {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.feature {
+    width: 45%;
+    margin-bottom: 10px;
+    font-size: 0.8em;
+    line-height: 1.4;
+}
+.cta {
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    margin: 0 auto;
+    margin-top: 20px;
+}
+
+
+.container3{
+    position: absolute;
+    margin:540px 80px;
+    width: 380px;
+    height: 500px;
+    justify-content: space-between;
+    border-radius: 5px;
+
+    display: block;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background:pink;
+
+
+}
+
+.container1{
+    position: absolute;
+    margin:540px 580px;
+    width: 380px;
+    height: 500px;
+    justify-content: space-between;
+    border-radius: 5px;
+  
+    display:block;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background:pink;
+ 
+
+}
+
+.container2{
+    position: absolute;
+    margin:540px 1080px;
+    width: 380px;
+    height: 500px;
+    justify-content: space-between;
+    border-radius: 5px;
+   
+    display: block;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background:pink;
+ 
 
 }
 
@@ -133,6 +233,51 @@ button:hover{
         <button class="main-button"><span></span>MORE INFORMATION</button>
     </div>
     </div>
+
+    <br>
+   
+    
+
+  
+    <div class="container3">
+            <img src="image/bag-removebg.png" alt="Women's Leather Bag" style="width: 100%; max-width: 400px; margin-bottom: 20px;">
+            <h1 class="title"> Women's Leather Bag</h1>
+            <p>Sale price: <span class="sale-price">$96.00</span> 
+            <ul class="features">
+                <li class="feature">Made of high-quality leather</li>
+                <li class="feature">Spacious interior with multiple compartments</li>
+                <li class="feature">Durable and stylish design</li>
+                <li class="feature">Perfect for work or travel</li>
+            </ul>
+            <a class="cta" href="#">Order Now</a>
+        </div>
+
+        <div class="container1">
+            <img src="image/rolex-png-rolex-watch-png-transparent-image-524__1_-removebg.png" alt="Women's Leather Bag" style="width: 100%; max-width: 400px; margin-bottom: 20px;">
+            <h1 class="title"> Women's Leather Bag</h1>
+            <p>Sale price: <span class="sale-price">$96.00</span> 
+            <ul class="features">
+                <li class="feature">Made of high-quality leather</li>
+                <li class="feature">Spacious interior with multiple compartments</li>
+                <li class="feature">Durable and stylish design</li>
+                <li class="feature">Perfect for work or travel</li>
+            </ul>
+            <a class="cta" href="#">Order Now</a>
+        </div>
+
+        <div class="container2">
+            <img src="image/j1 low gs.png" alt=" Hoodie" style="width: 100%; max-width: 400px; margin-bottom: 20px;">
+            <h1 class="title"> Men's Hoodie</h1>
+            <p>Sale price: <span class="sale-price">$45.00</span> 
+            <ul class="features">
+                <li class="feature">Made of high-quality leather</li>
+                <li class="feature">Spacious interior with multiple compartments</li>
+                <li class="feature">Durable and stylish design</li>
+                <li class="feature">Perfect for work or travel</li>
+            </ul>
+            <a class="cta" href="#">Order Now</a>
+        </div>
+        
 </main>
    
 
